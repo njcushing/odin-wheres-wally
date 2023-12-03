0a14bd84bfbf63715404b1b97c4be3f3117c24f7
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-03 11:35:52 +0000

    Import NavBar component into Landing page component

--- packages/frontend/src/pages/Landing/index.jsx
+++ packages/frontend/src/pages/Landing/index.jsx
@@ -1,10 +1,27 @@
 import styles from "./index.module.css";
 
+import NavBar from "@/features/NavBar";
+
 const Landing = () => {
+
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
             <h1 className={styles["title"]} aria-label="title">Where's Wally?</h1>
+            <div className={styles["nav-bar"]}>
+                <NavBar
+                    options={[
+                        {
+                            text: "Play",
+                            link: "/play",
+                        },
+                        {
+                            text: "High-Score Table",
+                            link: "/high-scores",
+                        },
+                    ]}
+                />
+            </div>
         </div>
         </div>
     )
