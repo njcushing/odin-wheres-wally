2773f8a0fbbff4b2920a1b5690ea0aea2f8cfbb0
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-03 12:56:40 +0000

    Set up basic layout for main game area of Play page component

--- packages/frontend/src/pages/Play/index.jsx
+++ packages/frontend/src/pages/Play/index.jsx
@@ -2,8 +2,16 @@ import styles from "./index.module.css";
 
 const Play = () => {
     return (
-        <>
-        </>
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+            <div className={styles["game-container"]}>
+                <div className={styles["image-container"]}>
+                </div>
+                <div className={styles["characters-found-container"]}>
+                </div>
+            </div>
+        </div>
+        </div>
     )
 };
 
