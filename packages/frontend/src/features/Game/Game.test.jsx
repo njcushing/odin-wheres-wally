5c04e0fdcbe373139a26bc64d2e5468959dc1b2c
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-04 21:36:32 +0000

    Add game image element and selection box to Game component

--- packages/frontend/src/features/Game/Game.test.jsx
+++ packages/frontend/src/features/Game/Game.test.jsx
@@ -11,6 +11,12 @@ const renderComponent = () => {
     render(<Game />);
 }
 
+const startGame = async (user) => {
+    renderComponent();
+    const startGameButton = screen.getByRole("button", { name: /Start Game/i });
+    await user.click(startGameButton);
+}
+
 const fetchCharacters = vi.fn(() => ["1", "2", "3"]);
 vi.mock('./utils/fetchAPI', () => ({
     fetchCharacters: () => fetchCharacters,
@@ -25,7 +31,8 @@ describe("UI/DOM Testing...", () => {
         });
     });
     describe("The list of the characters remaining...", () => {
-        test("Should contain the same number of children as characters returned from the server", () => {
+        test(`Should contain the same number of children as characters returned
+           from the server`, () => {
             renderComponent();
             const characters = screen.getAllByRole("listitem", { name: "character" });
             expect(characters.length).toBe(3);
@@ -39,10 +46,56 @@ describe("UI/DOM Testing...", () => {
         });
         test("When clicked, should be removed", async () => {
             const user = userEvent.setup();
+            await startGame(user);
+            const startGameButton = screen.queryByRole("button", { name: /Start Game/i });
+            expect(startGameButton).toBeNull();
+        });
+    });
+    describe("The Game Image...", () => {
+        test("Should not exist on the page before the game has started", () => {
             renderComponent();
-            const startGameButton = screen.getByRole("button", { name: /Start Game/i });
-            await user.click(startGameButton);
-            expect(startGameButton).not.toBeInTheDocument();
+            const gameImage = screen.queryByRole("img", { name: "Image containing the characters to locate." });
+            expect(gameImage).toBeNull();
+        });
+        test(`Should be loaded in when the 'Start Game' button has just been
+           clicked`, async () => {
+            const user = userEvent.setup();
+            await startGame(user);
+            const gameImage = screen.getByRole("img", { name: "Image containing the characters to locate." });
+            expect(gameImage).toBeInTheDocument();
+        });
+    });
+    describe("The Selection Box...", () => {
+        test("Should not exist on the page before the game has started", () => {
+            renderComponent();
+            const selectionBox = screen.queryByRole("generic", { name: "selection-area" });
+            expect(selectionBox).toBeNull();
+        });
+        test(`Should not exist on the page when the 'Start Game' button has just
+           been clicked`, async () => {
+            const user = userEvent.setup();
+            await startGame(user);
+            const selectionBox = screen.queryByRole("generic", { name: "selection-area" });
+            expect(selectionBox).toBeNull();
+        });
+        test(`Should be present when the game has started and the game image is
+           clicked`, async () => {
+            const user = userEvent.setup();
+            await startGame(user);
+            const gameImage = screen.getByRole("img", { name: "Image containing the characters to locate." });
+            await user.click(gameImage);
+            const selectionBox = screen.getByRole("generic", { name: "selection-area" });
+            expect(selectionBox).toBeInTheDocument();
+        });
+        test(`If it is present, it should be removed when the game image is
+           clicked`, async () => {
+            const user = userEvent.setup();
+            await startGame(user);
+            const gameImage = screen.getByRole("img", { name: "Image containing the characters to locate." });
+            await user.click(gameImage);
+            const selectionBox = screen.getByRole("generic", { name: "selection-area" });
+            await user.click(gameImage);
+            expect(selectionBox).not.toBeInTheDocument();
         });
     });
 });
--- packages/frontend/src/features/Game/index.jsx
+++ packages/frontend/src/features/Game/index.jsx
@@ -6,18 +6,56 @@ import * as fetchAPI from "./utils/fetchAPI.js";
 const Game = () => {
     const [gameStarted, setGameStarted] = useState(false);
     const [characters, setCharacters] = useState([]);
+    const [gameImage, setGameImage] = useState("");
+    const [selecting, setSelecting] = useState(false);
+    const [clickPosition, setClickPosition] = useState([0, 0]);
+
+    const boxSizePx = [72, 72];
 
     useEffect(() => {
         const characters = fetchAPI.fetchCharacters();
         setCharacters(characters);
     }, []);
 
+    const clickedImage = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const clickPos = [e.clientX - rect.left, e.clientY - rect.top];
+        setSelecting(!selecting);
+        setClickPosition(clickPos);
+    }
+
+    const clickedSelectionBox = () => {
+        return;
+    }
+
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
             <div className={styles["image-container"]}>
                 {gameStarted
-                ?   null
+                ?   <>
+                    <img
+                        className={styles["game-image"]}
+                        src={gameImage}
+                        alt="Image containing the characters to locate."
+                        onClick={clickedImage}
+                    ></img>
+                    {selecting
+                    ?   <div
+                            className={styles["selection-box"]}
+                            aria-label="selection-area"
+                            onClick={clickedSelectionBox}
+                            style={{
+                                position: "absolute",
+                                left: `${Math.max(clickPosition[0] - (boxSizePx[0] / 2), 0)}px`,
+                                top: `${Math.max(clickPosition[1] - (boxSizePx[1] / 2), 0)}px`,
+
+                                width: `${boxSizePx[0]}px`,
+                                height: `${boxSizePx[1]}px`,
+                            }}
+                        ></div>
+                    :   null}
+                    </>
                 :   <button
                         className={styles["start-game-button"]}
                         onClick={(e) => {
@@ -29,7 +67,7 @@ const Game = () => {
                             e.currentTarget.blur();
                         }}
                     >Start Game</button>
-                } 
+                }
             </div>
             <div className={styles["characters-remaining-container"]}>
                 <h3
