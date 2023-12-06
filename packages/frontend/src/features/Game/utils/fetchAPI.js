c999edf4cf74057d9693498a71a0811fe1acb53d
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-06 22:14:36 +0000

    Write new empty API functions
    One for fetching game information and the other for posting selection
    information

--- packages/frontend/src/features/Game/utils/fetchAPI.js
+++ packages/frontend/src/features/Game/utils/fetchAPI.js
@@ -1,3 +1,11 @@
-export const fetchCharacters = () => {
-    return [];
+export const getGameInformation = () => {
+    return {
+        image: null,
+        imageSize: [800, 800],
+        characters: ["char_1", "char_2", "char_3", "char_4", "char_5"],
+    };
+};
+
+export const postCharacterSelection = (character, clickPosition) => {
+    return;
 };
