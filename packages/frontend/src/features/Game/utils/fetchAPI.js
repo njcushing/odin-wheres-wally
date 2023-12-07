b9bfa110071566221521278b8430fd3d865170ac
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-07 10:00:11 +0000

    Write beginning of function that returns game duration string

--- packages/frontend/src/features/Game/utils/fetchAPI.js
+++ packages/frontend/src/features/Game/utils/fetchAPI.js
@@ -9,3 +9,17 @@ export const getGameInformation = () => {
 export const postCharacterSelection = (character, clickPosition) => {
     return true;
 };
+
+export const getGameDuration = () => {
+    const startDate = new Date("2023-12-07T14:43:11");
+    const endDate = new Date("2023-12-07T14:48:42");
+    let seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes - hours * 60;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds - minutes * 60;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${hours}:${minutes}:${seconds}`;
+};
