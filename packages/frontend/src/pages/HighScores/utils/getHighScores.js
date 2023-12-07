51374ba5bd1d7bc9432da0bf177642b6f7400760
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-07 10:45:19 +0000

    Write beginning of function that requests high scores

---
+++ packages/frontend/src/pages/HighScores/utils/getHighScores.js
@@ -0,0 +1,31 @@
+const getHighScores = () => {
+    return [
+        {
+            firstName: "Davey",
+            lastName: "Jones",
+            time: 213,
+        },
+        {
+            firstName: "Elizabeth",
+            lastName: "Swann",
+            time: 297,
+        },
+        {
+            firstName: "Jack",
+            lastName: "Sparrow",
+            time: 345,
+        },
+        {
+            firstName: "John",
+            lastName: "Smith",
+            time: 380,
+        },
+        {
+            firstName: "William",
+            lastName: "Turner",
+            time: 645,
+        },
+    ];
+};
+
+export default getHighScores;
