13cd1138d005b48be9b38a5e3e25fea783170833
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-08 10:26:17 +0000

    Update Game and Character schemas
    Needed to move the position and width/height of the characters into the Game schema since they are game-dependent properties

--- packages/backend/models/character.js
+++ packages/backend/models/character.js
@@ -3,11 +3,8 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const CharacterSchema = new Schema({
-    image: { type: String },
-    positionX: { type: Number },
-    positionY: { type: Number },
-    width: { type: Number },
-    height: { type: Number },
+    name: { type: String },
+    imageUrl: { type: String },
 });
 
 export default mongoose.model("Character", CharacterSchema);
--- packages/backend/models/game.js
+++ packages/backend/models/game.js
@@ -5,10 +5,18 @@ import CharacterSchema from "./character.js";
 const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
-    image: { type: String },
+    imageUrl: { type: String },
     imageWidth: { type: Number },
     imageHeight: { type: Number },
-    characters: [{ type: CharacterSchema }],
+    characters: [
+        {
+            character: { type: Schema.Types.ObjectId, ref: "Character" },
+            positionX: { type: Number },
+            positionY: { type: Number },
+            width: { type: Number },
+            height: { type: Number },
+        },
+    ],
 });
 
 export default mongoose.model("Game", GameSchema);
