97056d3ee6091567372e5b7444f8d5f3884134c9
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-10 13:29:29 +0000

    Remove redundant JWT functionality from backend

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -7,9 +7,6 @@ import cors from "cors";
 import passport from "passport";
 import passportLocal from "passport-local";
 const LocalStrategy = passportLocal.Strategy;
-import passportJWT from "passport-jwt";
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
 import { fileURLToPath } from "url";
 import path from "path";
 import logger from "morgan";
@@ -43,8 +40,6 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-import validateUserCredentials from "./utils/validateUserCredentials.js";
-
 // Verify current login details
 passport.use(
     "local",
@@ -60,33 +55,6 @@ passport.use(
             return done(err);
         }
     })
-);
-
-// Verify token
-passport.use(
-    "jwt",
-    new JWTStrategy(
-        {
-            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-            secretOrKey: process.env.AUTH_SECRET_KEY,
-        },
-        async (jwt_payload, done) => {
-            const { username, password } = jwt_payload.user;
-            try {
-                const [status, user, msg] = await validateUserCredentials(
-                    username,
-                    password
-                );
-                if (!status) return done(null, false, { message: msg });
-                if (!user.admin) {
-                    return done(null, false, { message: "Unauthorised user." });
-                }
-                return done(null, true);
-            } catch (err) {
-                return done(err);
-            }
-        }
-    )
 );
 
 app.use(logger("dev"));
