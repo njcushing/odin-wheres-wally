55940e439692ee417afefecc55d85170f97ac6f8
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-14 09:54:45 +0000

    Change time conversion module to handle millisecond input instead of seconds

---
+++ packages/frontend/src/utils/convertTimeMillisecondsToHours.js
@@ -0,0 +1,25 @@
+const convertTimeMillisecondsToHours = (milliseconds) => {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes - hours * 60;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds - minutes * 60;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    milliseconds = milliseconds - seconds * 1000;
+    milliseconds =
+        milliseconds < 100
+            ? milliseconds < 10
+                ? `00${milliseconds}`
+                : `0${milliseconds}`
+            : milliseconds;
+    return {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+        milliseconds: milliseconds,
+    };
+};
+
+export default convertTimeMillisecondsToHours;
